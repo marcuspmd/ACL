64e2967eac9cb87f39662e2de53f70d8221b2518
Marcus Paulo M Dias <scorpionslanhouse@gmail.com>
2014-09-19 12:10:01 -0300

    merge and request pull of armando

--- models/acl.php
+++ models/acl.php
@@ -22,32 +22,16 @@ class acl extends CI_Model {
             $white_list = array();
             $white_list[] = 'validation';
             $url = $this->uri->uri_string();
+            ($this->uri->total_segments() == 2)? $url.'/' : $url;
 
-<<<<<<< HEAD
             if ($this->validate_white_list($url))
               return TRUE;
-=======
-            if (in_array($url, $white_list)) {
-                return TRUE;
-            }
-
-            $this->db->select('module.description as module_name, routine.name as menu_nome, routine.access_key, routine.link');
-            $this->db->join('routine', 'module.id_module = routine.module_id_module');
-            $this->db->join('permission', 'routine.id_routine = permission.routine_id_routine');
-            $this->db->where("'$url'".' regexp ', 'routine.link', false);
-            $this->db->where('permission.user_id_user', $ACL_USER);
-            $query = $this->db->get('module');
-            if ($query->num_rows == 0) {
-                $this->session->set_flashdata('msg', 'Seu usuário atual não possui permissões para acessar a página solicitada.');
-                redirect($this->page_redirect);
-            }
->>>>>>> a074702ba7aef71609bfe230b1f3b833f7c86163
 
             return $this->validate_permission($ACL_USER, $url);
         }
     }
 
-<<<<<<< HEAD
+
     private function validate_white_list($url){
         $this->db->select('routine.link');
         $this->db->where('routine.link', $url);
@@ -73,6 +57,4 @@ class acl extends CI_Model {
     }
 
 }
-=======
-}
->>>>>>> a074702ba7aef71609bfe230b1f3b833f7c86163
+
